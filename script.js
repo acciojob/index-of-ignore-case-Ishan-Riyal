550f994a52b9a72276e44ecd055c4dd496cf0080
Ishan Riyal <ishanriyal07@gmail.com>
2025-05-31 18:10:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+  if (typeof str !== 'string' || typeof subStr !== 'string') return -1;
+  return str.toLowerCase().indexOf(subStr.toLowerCase());
 }
-
+ 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
